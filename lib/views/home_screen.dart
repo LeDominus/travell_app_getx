4bb1297e02569c491e3e61904db4e149fc569c63
LeDominus <kalyuzhin2004@yandex.ru>
2024-08-31 16:41:58 +0700

    starting day 2

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -10,9 +10,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    int currentIndex = 0;
     List<String> images = [
       "assets/images/1.jpg",
       "assets/images/2.jpg",
@@ -23,64 +24,70 @@ class _HomeScreenState extends State<HomeScreen> {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[50],
-          leading: Container(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[50],
+        leading: Container(
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(30)),
+          padding: const EdgeInsets.all(5),
+          child: const Icon(
+            Icons.dashboard,
+            size: 30,
+            color: Colors.grey,
+          ),
+        ),
+        actions: [
+          Container(
+            alignment: Alignment.center,
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(30)),
-            padding: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(10),
             child: const Icon(
-              Icons.dashboard,
+              CupertinoIcons.bell,
               size: 30,
               color: Colors.grey,
             ),
           ),
-          actions: [
-            Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(30)),
-              padding: const EdgeInsets.all(10),
-              child: const Icon(
-                CupertinoIcons.bell,
-                size: 30,
-                color: Colors.grey,
-              ),
-            ),
-          ],
-        ),
-        body: SizedBox(
-          width: width,
-          height: height,
-          child: Stack(
-            children: [
-              Positioned(
-                  top: 0.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: SizedBox(
-                      height: height,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: height * .05,
-                          ),
-                          SizedBox(
-                            width: width,
-                            height: height * .3,
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                    child: Container(
-                                  width: width,
-                                  height: height * .3,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
+        ],
+      ),
+      body: SizedBox(
+        width: width,
+        height: height,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                  height: height,
+                  child: Column(
+                    children: [
+                      SizedBox(height: height * .05),
+                      SizedBox(
+                        width: width,
+                        height: height * .3,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              child: Container(
+                                width: width,
+                                height: height * .3,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
                                   child: PageView.builder(
                                     itemCount: images.length,
+                                    physics: const ClampingScrollPhysics(),
+                                    onPageChanged: (value) {
+                                      setState(() {
+                                        currentIndex = value;
+                                      });
+                                    },
                                     itemBuilder: (context, index) {
                                       return ClipRRect(
                                         borderRadius: BorderRadius.circular(15),
@@ -91,67 +98,102 @@ class _HomeScreenState extends State<HomeScreen> {
                                       );
                                     },
                                   ),
-                                )),
-                                const Positioned(
-                                    top: 25,
-                                    left: 25,
-                                    child: Text("Welcome to our tour",
-                                        style: TextStyle(
-                                          fontFamily: 'Aclonica',
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.w700,
-                                          color: Colors.white,
-                                        )))
-                              ],
+                                ),
+                              ),
                             ),
-                          )
-                        ],
+                            const Positioned(
+                              top: 25,
+                              left: 25,
+                              child: Text(
+                                "Welcome to our tour",
+                                style: TextStyle(
+                                  fontFamily: 'Aclonica',
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 10,
+                              left: 0,
+                              right: 0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: List.generate(
+                                  images.length,
+                                  (index) => AnimatedContainer(
+                                    duration: const Duration(milliseconds: 500),
+                                    width: currentIndex == index ? 20 : 10,
+                                    height: 10,
+                                    margin: const EdgeInsets.only(right: 5),
+                                    decoration: BoxDecoration(
+                                      color: currentIndex == index
+                                          ? Colors.deepPurple
+                                          : Colors.white,
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )),
-              Positioned(
-                  bottom: 0.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: Container(
-                    width: width,
-                    color: Colors.blue[50],
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const Icon(
-                            Icons.home_filled,
-                            size: 35,
+                      Expanded(
+                        child:
+                            Container(), // Placeholder for additional content
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                width: width,
+                color: Colors.blue[50],
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Icon(
+                        Icons.home_filled,
+                        size: 35,
+                        color: Colors.deepPurple,
+                      ),
+                      const Icon(
+                        Icons.search,
+                        size: 35,
+                        color: Colors.grey,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
                             color: Colors.deepPurple,
-                          ),
-                          const Icon(
-                            Icons.search,
-                            size: 35,
-                            color: Colors.grey,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                color: Colors.deepPurple,
-                                borderRadius: BorderRadius.circular(30)),
-                            padding: const EdgeInsets.all(10),
-                            child: const Icon(
-                              Icons.explore,
-                              size: 30,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const Icon(Icons.bookmark_outline,
-                              size: 35, color: Colors.grey),
-                          const Icon(Icons.person_outline,
-                              size: 35, color: Colors.grey)
-                        ],
+                            borderRadius: BorderRadius.circular(30)),
+                        padding: const EdgeInsets.all(10),
+                        child: const Icon(
+                          Icons.explore,
+                          size: 30,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ))
-            ],
-          ),
-        ));
+                      const Icon(Icons.bookmark_outline,
+                          size: 35, color: Colors.grey),
+                      const Icon(Icons.person_outline,
+                          size: 35, color: Colors.grey)
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
